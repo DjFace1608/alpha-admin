c142aa165e98aad2bcf813c3cf7f1680f80fce98
DjFace1608 <spyros@alphagmbh.berlin>
2025-06-02 15:23:06 +0200

    Create ProductList.js

---
+++ src/components/ProductList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://alpha-backend-d0r9.onrender.com/api/products")
+      .then(res => setProducts(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Produktliste</h2>
+      <ul>
+        {products.map(p => (
+          <li key={p._id}>{p.name} ({p.unit})</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;

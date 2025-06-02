50acdf449d0596540999c68757361fc9bb985bef
DjFace1608 <spyros@alphagmbh.berlin>
2025-06-02 16:13:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,3 +12,20 @@ function App() {
   );
 }
 export default App;
+import React from 'react';
+import ProductList from './components/ProductList';
+import ProductImport from './components/ProductImport';
+import ProductImportExcel from './components/ProductImportExcel';
+function App() {
+  return (
+    <div className="App">
+      <h1>Alpha Admin Panel</h1>
+      <ProductImportExcel />
+      <hr />
+      <ProductImport />
+      <hr />
+      <ProductList />
+    </div>
+  );
+}
+export default App;

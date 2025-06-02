4913eb1db4405b0e54b1ec7783690f836f8c07dc
DjFace1608 <spyros@alphagmbh.berlin>
2025-06-02 15:40:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ProductList from './components/ProductList';
+import ProductImport from './components/ProductImport';
 
 function App() {
   return (
     <div className="App">
       <h1>Alpha Admin Panel</h1>
+      <ProductImport />
+      <hr />
       <ProductList />
     </div>
   );
